c751d60736c73a617e7a69f1e2309410d28fb2a9
Vitor Hugo <vitorh565.vh@gmail.com>
2025-04-07 14:57:40 -0300

    POO

---
+++ Aula6_27_03/Aula6_27_03/Program.cs
@@ -0,0 +1,21 @@
+﻿// Programação orientada a objeto
+class Program
+{
+    class Carro  // Isso é uma Classe
+    {
+        // Estes são os atributos do objeto Carro
+        public string modelo;
+        public int ano;
+    }
+    static void Main()
+    {
+        Carro meuCarro = new Carro(); // Objeto
+        meuCarro.modelo = "Fusca"; // Atribuindo ao objeto
+        meuCarro.ano = 1980;
+
+        Console.WriteLine($"Modelo: {meuCarro.modelo}");
+        Console.WriteLine($"Ano: {meuCarro.ano}");
+    }
+}
+
+
